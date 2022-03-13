22c2484c27744946dddf9f2ef96a3e8dfd099e61
Александр Кинцлер <alexander.kintsler@yandex.ru>
2022-03-13 23:40:25 +0300

    Правки

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,37 +1,53 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
 
 class Group(models.Model):
+    """В базе данных создается модель для добавления групп,
+    в которых будут публиковаться записи(посты)."""
+
     title = models.CharField(
-        max_length=200,
-        verbose_name='Имя'
+        verbose_name='Имя',
+        max_length=200
     )
     slug = models.SlugField(verbose_name='Адрес', unique=True)
     description = models.TextField(verbose_name='Описание')
+
+    class Meta:
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
 
     def __str__(self) -> str:
         return self.title
 
 
 class Post(models.Model):
+    """В базе данных создается модель для добавления записей(постов),
+    они будут публиковаться в группах(модель Group)."""
+
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Дата публикации'
+        verbose_name='Дата публикации',
+        auto_now_add=True
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='post',
+        related_name='posts',
         verbose_name='Автор'
     )
     group = models.ForeignKey(
         Group,
-        on_delete=models.CASCADE,
-        related_name='group_posts',
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        verbose_name='Группа',
         blank=True,
         null=True
     )
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+        ordering = ['-pub_date']
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
 
+POSTS_PER_PAGE = 10
+
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:POSTS_PER_PAGE]
     context = {
         'posts': posts,
     }
@@ -13,7 +15,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()[:POSTS_PER_PAGE]
     context = {
         'group': group,
         'posts': posts,
