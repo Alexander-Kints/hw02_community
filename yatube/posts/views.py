c6c44c7f97dafbc340c9b54aec90ae1c66206bae
Александр Кинцлер <alexander.kintsler@yandex.ru>
2022-03-23 22:23:58 +0300

    context_processors & paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
@@ -6,9 +7,12 @@ POSTS_PER_PAGE = 10
 
 
 def index(request):
-    posts = Post.objects.all()[:POSTS_PER_PAGE]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
